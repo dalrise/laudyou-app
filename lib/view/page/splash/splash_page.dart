ede7ff816f2d69958205b7dc8cb6bb1a09266f75
JO <johyun0218@naver.com>
2022-02-08 16:21:55 +0900

    fix: splash lottie 적용

--- lib/view/page/splash/splash_page.dart
+++ lib/view/page/splash/splash_page.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:laudyou_app/constants.dart';
 import 'package:laudyou_app/view/page/main_home_page.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -16,33 +17,46 @@ class SplashPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: kPrimaryColor,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 5,
-              child: Center(
-                child: Text(
-                  "Laud You....",
-                  style: GoogleFonts.aladin(fontSize: 35, color: Colors.white),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: EdgeInsets.all(24),
-                child: TextButton(
-                  child: const Text("Continue"),
-                  onPressed: () {
-                    Get.off(() => const MainHomePage(title: "Laud You"));
-                  },
-                ),
-              ),
-            )
-          ],
-        ),
+        child: _buildLottie(),
       ),
+    );
+  }
+
+  Widget _buildLottie() {
+    return Center(
+      child: ListView(shrinkWrap: true, children: [
+        // Load a Lottie file from your assets
+        Lottie.asset('assets/lottie/95074-hamburger-loading.json'),
+      ]),
+    );
+  }
+
+  Column _buildColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Center(
+            child: Text(
+              "Laud You....",
+              style: GoogleFonts.aladin(fontSize: 35, color: Colors.white),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Padding(
+            padding: EdgeInsets.all(24),
+            child: TextButton(
+              child: const Text("Continue"),
+              onPressed: () {
+                Get.off(() => const MainHomePage(title: "Laud You"));
+              },
+            ),
+          ),
+        )
+      ],
     );
   }
 }
